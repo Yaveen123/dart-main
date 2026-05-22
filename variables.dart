5b041c74910e4e6e8f5f6af1b8c74331fb374f92
Yav <94953863+Yaveen123@users.noreply.github.com>
2026-05-22 20:24:39 +1000

    Finished variables section in Dart
    Add variables.dart demonstrating basic Dart variable usage for the tutorial: shows var string reassignment, dynamic typing, const and final declarations, and a print statement. Serves as a simple starter for learning Dart variables.

---
+++ variables.dart
@@ -0,0 +1,13 @@
+// In the tutorial, we began by learning variables
+
+void main() {
+  // Strings
+  var name = "Yav!";
+  name = "Yavi";
+
+  dynamic my_value = 1;
+
+  const int my_constant_int = 10;
+  final dynamic thing = true;
+  print(thing);
+}

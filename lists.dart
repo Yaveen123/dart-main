ed912a767caa1f0f1e8c1ab56a5209ebc4e40860
Yav <94953863+Yaveen123@users.noreply.github.com>
2026-05-22 21:24:44 +1000

    Finished maps section

--- lists.dart
+++ lists.dart
@@ -13,6 +13,8 @@ void main() {
   myList.addAll([020,10,3]);
 
   // insert at specific position
-  myList.insert(2, "?");
+  myList.insert(2, "string");
   print(myList);
+
+  myList.insertAll(2, [2,13,12,1]);
 }
---
+++ maps.dart
@@ -0,0 +1,16 @@
+void main() {
+  var my_map = {
+    "key_1": "val1",
+    "key_2": "val2"
+  };
+
+  print(my_map);
+  print(my_map['key_1']);
+  
+  // get values and keys
+  print(my_map.values);
+  print(my_map.keys);
+
+  print(my_map.length);
+
+}
